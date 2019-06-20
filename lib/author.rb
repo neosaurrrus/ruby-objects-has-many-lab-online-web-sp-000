27322a2904e31c4411a95afd68bd82baa3b3fc28
Lukie Kang <lukiek+f@gmail.com>
2019-06-20 21:57:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,6 +15,11 @@ attr_accessor :name, :posts
     @posts << post
   end
 
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self
+    @posts << new_post
+
 
 
 
