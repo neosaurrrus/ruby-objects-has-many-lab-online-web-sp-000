632f7fc7f9d377a256c21dbad724a7124f305361
Lukie Kang <lukiek+f@gmail.com>
2019-06-20 22:05:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,19 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end # of initialize
 
   def artist
     @artist
   end
 
-
+  def self.all
+    @@all
+  end #of all
 
 end # of class
