2f4875d4e5d269c29d0f9a119c137ff3783fa86b
Lukie Kang <lukiek+f@gmail.com>
2019-06-20 21:55:47 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,10 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
     def initialize(title)
       @title = title
     end
 
+    def author
+      @author
+    end
 end #of Post
