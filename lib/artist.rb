89f5942bd04b5bc82fc2d5edbeeea0dcbb47d179
Lukie Kang <lukiek+f@gmail.com>
2019-06-20 21:47:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist
   attr_accessor :name, :songs
-
+  @@all_songs.length = 0
   def initialize(name)
     @name = name
     @songs = []
-    @@all_songs.length = 0
+
   end
 
   def songs
